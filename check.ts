b4636c0408ebbddb40ae833b27e32850cd14fb44
Ole Barsch <ole.oystein.barsch@nrk.no>
2023-06-08 10:03:27 +0200

    no space

--- check.ts
+++ check.ts
@@ -1,6 +1,6 @@
-import path from 'path';
-import { NotificationCenter } from 'node-notifier';
-import { addMilliseconds, intervalToDuration } from 'date-fns';
+import path from "path";
+import { NotificationCenter } from "node-notifier";
+import { addMilliseconds, intervalToDuration } from "date-fns";
 
 type TicketResponse = { groups: any[]; offers: any[] };
 
@@ -8,11 +8,10 @@ const notificationCenterNotifier = new NotificationCenter({
   withFallback: true,
 });
 
-const baseUrl = 'https://availability.ticketmaster.eu/api/v2/TM_NO/resale';
-const ticketmasterUrl =
-  'https://www.ticketmaster.no/artist/vinjerock-billetter/899820';
-const ordinaryId = '703709';
-const dntId = '705713';
+const baseUrl = "https://availability.ticketmaster.eu/api/v2/TM_NO/resale";
+const ticketmasterUrl = "https://www.ticketmaster.no/artist/vinjerock-billetter/899820";
+const ordinaryId = "703709";
+const dntId = "705713";
 
 const fetchTickets = async (id: string) => {
   const response = await fetch(`${baseUrl}/${id}`);
@@ -32,7 +31,7 @@ const sendNotification = (title: string, message: string, open?: string) => {
       title,
       message,
       // icon: path.join(__dirname, 'vinje-logo.png'),
-      contentImage: path.join(__dirname, 'vinje-logo.png'),
+      contentImage: path.join(__dirname, "vinje-logo.png"),
       open,
       timeout: 10_000,
     },
@@ -48,9 +47,9 @@ let nextUpdate: Date = new Date();
 
 const showTimeToNextUpdate = () => {
   const distance = intervalToDuration({ start: new Date(), end: nextUpdate });
-  const text = `Next update in ${distance.minutes} minute${
-    distance.minutes !== 1 ? 's' : ''
-  } and ${distance.seconds} second${distance.seconds !== 1 ? 's' : ''}`;
+  const text = `Next update in ${distance.minutes} minute${distance.minutes !== 1 ? "s" : ""} and ${
+    distance.seconds
+  } second${distance.seconds !== 1 ? "s" : ""}`;
   process.stdout.cursorTo(0);
   process.stdout.write(text);
 };
@@ -58,32 +57,24 @@ const showTimeToNextUpdate = () => {
 let nextUpdateInterval: ReturnType<typeof setInterval>;
 const checkForTickets = async () => {
   clearInterval(nextUpdateInterval);
-  console.log('\nChecking for tickets...');
+  console.log("\nChecking for tickets...");
   const ordinaryTickets = await fetchTickets(ordinaryId);
   const dntTickets = await fetchTickets(dntId);
 
   const hasOrdinaryTickets = hasTickets(ordinaryTickets);
   const hasDNTTickets = hasTickets(dntTickets);
   if (hasOrdinaryTickets) {
-    console.log('found ordinary tickets!');
-    sendNotification(
-      'Vinjerock',
-      'Vanlig billett tilgjengelig! Trykk for å komme til tickemaster',
-      ticketmasterUrl
-    );
+    console.log("found ordinary tickets!");
+    sendNotification("Vinjerock", "Vanlig billett tilgjengelig! Trykk for å komme til tickemaster", ticketmasterUrl);
   } else {
-    console.log('found no ordinary tickets');
+    console.log("found no ordinary tickets");
   }
 
   if (hasDNTTickets) {
-    console.log('found DNT tickets!');
-    sendNotification(
-      'Vinjerock',
-      'DNT-billett tilgjengelig! Trykk for å komme til tickemaster',
-      ticketmasterUrl
-    );
+    console.log("found DNT tickets!");
+    sendNotification("Vinjerock", "DNT-billett tilgjengelig! Trykk for å komme til tickemaster", ticketmasterUrl);
   } else {
-    console.log('found no DNT tickets');
+    console.log("found no DNT tickets");
   }
 
   const delay = Math.round(Math.random() * 1_000) * 60;
@@ -100,5 +91,5 @@ const checkForTickets = async () => {
   nextUpdateInterval = setInterval(showTimeToNextUpdate, 1000);
 };
 
-sendNotification('Vinje rock', 'Ser etter billetter alle 2ish minutter');
+sendNotification("Vinjerock", "Ser etter billetter alle 2ish minutter");
 checkForTickets();
